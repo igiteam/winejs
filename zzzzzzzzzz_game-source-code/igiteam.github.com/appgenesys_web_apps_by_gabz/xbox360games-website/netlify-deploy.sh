b77e5c5552bb92f67232926bc2166d746efdd9c4
IGI Team <igiteam.remake@gmail.com>
2026-05-02 17:07:26 +0100

    fixes

---
+++ zzzzzzzzzz_game-source-code/igiteam.github.com/appgenesys_web_apps_by_gabz/xbox360games-website/netlify-deploy.sh
@@ -0,0 +1,18 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python3 netlify_sites.py
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NODE_OPTIONS=--openssl-legacy-provider
+node -v
+npm -v
+#netlify deploy --prod --site your_site_id
+netlify deploy --prod --site 6a6d02d3-7cbd-43c6-8fda-da934eb71195
+
+git add .
+git commit -m 'fixes'
+git push
---
+++ zzzzzzzzzz_game-source-code/igiteam.github.com/appgenesys_web_apps_by_gabz/xbox360games-website/netlify-get_site_id.sh
@@ -0,0 +1,8 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NODE_OPTIONS=--openssl-legacy-provider
+node -v
+npm -v
+
+netlify sites:list
