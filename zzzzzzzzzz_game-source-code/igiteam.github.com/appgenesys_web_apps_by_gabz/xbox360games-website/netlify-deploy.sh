710eda4ec9f93028ec86485eda198901317cec12
IGI Team <igiteam.remake@gmail.com>
2026-05-02 17:07:41 +0100

    fixes

--- zzzzzzzzzz_game-source-code/igiteam.github.com/appgenesys_web_apps_by_gabz/xbox360games-website/netlify-deploy.sh
+++ zzzzzzzzzz_game-source-code/igiteam.github.com/appgenesys_web_apps_by_gabz/xbox360games-website/netlify-deploy.sh
@@ -1,9 +1,3 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python3 netlify_sites.py
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
